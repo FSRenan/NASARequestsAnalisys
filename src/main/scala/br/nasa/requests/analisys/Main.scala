bb140e66d92831fdc7e8a697bbace9b94835c51a
FSRenan <renan.022@hotmail.com>
2020-01-09 22:25:42 -0200

    Simple word count case

--- src/main/scala/br/nasa/requests/analisys/Main.scala
+++ src/main/scala/br/nasa/requests/analisys/Main.scala
@@ -1,9 +1,20 @@
 package br.nasa.requests.analisys
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
   def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("wordCounts").setMaster("local[3]")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("files/input/word_count.txt")
+    val words = lines.flatMap(line => line.split(" "))
+
+    val wordCounts = words.countByValue()
+    for ((word, count) <- wordCounts) println(word + " : " + count)
 
   }
 }
